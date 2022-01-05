9e51b0ea850862118c2a92123d1a5f5a0bb1715d
flo-kei <florian.keintzel@gmx.at>
2022-01-05 01:09:19 +0100

    small grpc service update, it now displays the amount of bookings per person

--- backend/src/main/java/at/flokei/boundary/HelloService.java
+++ backend/src/main/java/at/flokei/boundary/HelloService.java
@@ -3,16 +3,25 @@ package at.flokei.boundary;
 import at.flokei.Greeter;
 import at.flokei.HelloReply;
 import at.flokei.HelloRequest;
+import at.flokei.control.BookingRepository;
 import io.quarkus.grpc.GrpcService;
 import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
+import io.smallrye.mutiny.vertx.AsyncResultUni;
+
+import javax.inject.Inject;
 
 @GrpcService
 public class HelloService implements Greeter {
 
+    @Inject
+    BookingRepository bookingRepository;
+
     @Override
     public Uni<HelloReply> sayHello(HelloRequest request) {
-        return Uni.createFrom().item(() ->
-                HelloReply.newBuilder().setMessage("Hello " + request.getName()).build()
-        );
+        return Uni.createFrom().item(() -> {
+            return bookingRepository.getBookingsOfCustomer(request.getName()).size(); // Panache entity
+        }).runSubscriptionOn(Infrastructure.getDefaultExecutor())
+                .onItem().transform(i -> HelloReply.newBuilder().setMessage("Bookings of " + request.getName() +": "+Integer.toString(i)).build());
     }
 }
